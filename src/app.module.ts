62d8cb3ef3408a0a82c7241bc7c304ef34e1e344
Ankan Paul Turza <ankanp21@gmail.com>
2025-08-04 02:46:55 +0600

    .env file added
    environment variable for dabatase credentials added implement
    ed with ConfigMofule and ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LearnerModule } from './learner/learner.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [LearnerModule, TypeOrmModule.forRoot({
-    type : 'postgres',
-    host : 'localhost',
-    port : 5432,
-    username : 'postgres',
-    database : 'skillsync',
-    password : 'root',
-    autoLoadEntities : true,
-    synchronize : true
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    LearnerModule, 
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
